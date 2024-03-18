d6e3be6c18a18a26bc11414b26319540b2a6abdb
Ananias123 <omarmudacumura7@gmail.com>
2024-03-18 11:02:28 +0200

    Task 1 python Retrieve an element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
+def element_at(my_list, idx): # A function that retrieve tha element at agiven index in a list
     if idx < 0:
         return (None)
     if idx > len(my_list) - 1:
