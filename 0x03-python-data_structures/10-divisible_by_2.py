7bc96ede1a5269b8d4537f6c5f448af2a1fe33e6
Ananias123 <omarmudacumura7@gmail.com>
2024-03-18 13:20:47 +0200

    Print all multiples of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     Leng = len(my_list)
-    List_1 = []
+    list_new = []
     for a in range(Leng):
         if my_list[a] % 2 == 0:
-            List_1.append(True)
+            list_new.append(True)
         else:
-            List_1.append(False)
-    return List_1
+            list_1.append(False)
+    return list_new
 
