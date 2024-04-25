5e5922d98f581b5598e4e50d2237673800d4d297
Ananias123 <omarmudacumura7@gmail.com>
2024-04-26 01:20:07 +0200

    The first class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 '''this is the class named Base'''
+
+
 class Base:
 '''Private class attribute'''
     __nb_objects = 0
+
     def __init__(self, id=None):
         '''The class constructor'''
         if id != None:
